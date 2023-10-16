edb90ad65f201404725f1f3e840f1bf0ba4ce6de
Valentine Goh <80378682+valentineg-fz@users.noreply.github.com>
2023-10-16 14:58:49 +1100

    Upgrade notarytool (#4)
    * refactor notarization logic to use notarytool
    
    * temp change go mod
    
    * revert changes to go.mod

--- output.go
+++ output.go
@@ -3,13 +3,13 @@ package main
 import "time"
 
 type CommandOutput struct {
-	OSVersion string `plist:"os-version"`
-	SuccessMessage string `plist:"success-message"`
-	ToolPath string `plist:"tool-path"`
-	ToolVersion string `plist:"tool-version"`
-	ProductErrors []ProductError `plist:"product-errors"`
-	Upload NotarizationUpload `plist:"notarization-upload"`
-	Info NotarizationInfo `plist:"notarization-info"`
+	OSVersion      string             `plist:"os-version"`
+	SuccessMessage string             `plist:"success-message"`
+	ToolPath       string             `plist:"tool-path"`
+	ToolVersion    string             `plist:"tool-version"`
+	ProductErrors  []ProductError     `plist:"product-errors"`
+	Upload         NotarizationUpload `plist:"notarization-upload"`
+	Info           NotarizationInfo   `plist:"notarization-info"`
 }
 
 type NotarizationUpload struct {
@@ -17,18 +17,18 @@ type NotarizationUpload struct {
 }
 
 type NotarizationInfo struct {
-	Date time.Time `plist:"Date"`
-	Hash string `plist:"Hash"`
-	LogFileURL string `plist:"LogFileURL"`
-	RequestUUID string `plist:"RequestUUID"`
-	Status string `plist:"Status"`
-	StatusCode int `plist:"Status Code"`
-	StatusMessage string `plist:"Status Message"`
+	Date          time.Time `plist:"Date"`
+	Hash          string    `plist:"Hash"`
+	LogFileURL    string    `plist:"LogFileURL"`
+	RequestUUID   string    `plist:"RequestUUID"`
+	Status        string    `plist:"Status"`
+	StatusCode    int       `plist:"Status Code"`
+	StatusMessage string    `plist:"Status Message"`
 }
 
 type ProductError struct {
-	Code int `plist:"code"`
-	Message string `plist:"message"`
+	Code     int               `plist:"code"`
+	Message  string            `plist:"message"`
 	UserInfo map[string]string `plist:"userInfo"`
 }
 
@@ -37,29 +37,35 @@ func (error ProductError) Error() string {
 }
 
 type NotarizationLog struct {
-	JobID string `json:"jobId"`
-	Status string `json:"status"`
-	StatusSummary string `json:"statusSummary"`
-	StatusCode int `json:"statusCode"`
-	ArchiveFilename string `json:"archiveFilename"`
-	UploadDate string `json:"uploadDate"`
-	SHA256 string `json:"sha256"`
-	TicketContents []NotarizationTicket `json:"ticketContents"`
-	Issues []NotarizationIssue `json:"issues"`
+	JobID           string               `json:"jobId"`
+	Status          string               `json:"status"`
+	StatusSummary   string               `json:"statusSummary"`
+	StatusCode      int                  `json:"statusCode"`
+	ArchiveFilename string               `json:"archiveFilename"`
+	UploadDate      string               `json:"uploadDate"`
+	SHA256          string               `json:"sha256"`
+	TicketContents  []NotarizationTicket `json:"ticketContents"`
+	Issues          []NotarizationIssue  `json:"issues"`
 }
 
 type NotarizationTicket struct {
-	Path string `json:"path"`
+	Path            string `json:"path"`
 	DigestAlgorithm string `json:"digestAlgorithm"`
-	CDHash string `json:"cdhash"`
-	Arch string `json:"arch"`
+	CDHash          string `json:"cdhash"`
+	Arch            string `json:"arch"`
 }
 
 type NotarizationIssue struct {
-	Severity string `json:"severity"`
-	Code string `json:"code"`
-	Path string `json:"path"`
-	Message string `json:"message"`
-	DocUrl string `json:"docUrl"`
+	Severity     string `json:"severity"`
+	Code         string `json:"code"`
+	Path         string `json:"path"`
+	Message      string `json:"message"`
+	DocUrl       string `json:"docUrl"`
 	Architecture string `json:"architecture"`
+}
+
+type NotaryToolInfo struct {
+	RequestUUID string `plist:"id"`
+	Message     string `plist:"message"`
+	Status      string `plist:"status"`
 }
--- worker.go
+++ worker.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
@@ -153,74 +152,6 @@ func (worker *Worker) addFileToZIP(writer *zip.Writer, info os.FileInfo, path st
 		if _, err := io.Copy(fileWriter, sourceFile); err != nil {
 			return errors.Wrap(err, "write ZIP entry data from file")
 		}
-	}
-
-	return nil
-}
-
-func (worker *Worker) uploadAndWait() {
-	defer worker.wait.Done()
-
-	worker.logger.Info().Msg("Uploading package to Apple notarization service")
-	upload, err := worker.uploadForNotarization()
-	if err != nil {
-		worker.logger.Error().Err(err).Msg("Upload to Apple failed")
-		return
-	}
-
-	worker.logger.Info().Str("request-id", upload.RequestUUID).Msg("Successfully uploaded package, waiting for notarization to complete")
-
-waitLoop:
-	for {
-		info, err := worker.getNotarizationStatus(upload)
-		switch {
-		case err != nil && errors.Cause(err).Error() == "Could not find the RequestUUID.":
-			time.Sleep(infoWaitTime)
-
-		case err != nil:
-			worker.logger.Error().Err(err).Msg("Encountered error getting notarization status from Apple")
-			return
-
-		case info.Status == "in progress":
-			time.Sleep(infoWaitTime)
-
-		case info.Status == "invalid":
-			worker.info = info
-			worker.logger.Error().Msg("Notarization failed, please see the log file for more information")
-			break waitLoop
-
-		default:
-			worker.logger.Info().Msg("Notarization completed successfully")
-			worker.info = info
-			break waitLoop
-		}
-	}
-
-	if worker.info.Status != "invalid" {
-		if err := worker.staplePackage(); err != nil {
-			worker.logger.Error().Err(err).Msg("Failed to staple notarization to package")
-		}
-	}
-
-	worker.logger.Info().Msg("Retrieving notarization log from Apple")
-	if err := worker.downloadNotarizationLog(); err != nil {
-		worker.logger.Error().Err(err).Msg("Failed to download notarization log")
-	}
-
-	worker.logger.Info().Msg("Notarization finished")
-}
-
-func (worker *Worker) downloadNotarizationLog() error {
-	req, _ := http.NewRequest(http.MethodGet, worker.info.LogFileURL, nil)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return errors.Wrap(err, "request notarization log")
-	}
-	defer resp.Body.Close()
-
-	worker.log = new(NotarizationLog)
-	if err := json.NewDecoder(resp.Body).Decode(worker.log); err != nil {
-		return errors.Wrap(err, "unmarshal notarization log")
 	}
 
 	return nil
